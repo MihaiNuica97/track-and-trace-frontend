225dade25f8b467cbc6a0322b201bcba206d20e5
MihaiNuica97 <mihainuica@gmail.com>
2020-11-15 16:26:55 +0000

    checkins properly implemented

--- js/checkin.js
+++ js/checkin.js
@@ -5,24 +5,70 @@ $(document).ready(() => {
 	venue = JSON.parse(localStorage.getItem("venueToCheckIn"));
 	venueNameHTML.text(venue.Name);
 	console.log(venue);
+	let today = new Date();
+	let dateString =
+		today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+	$("#checkindate").val(dateString);
 });
 
 function checkIn() {
-	// let venueName = $("#venuename").val().trim();
+	// grab date
+	let checkinDate = $("#checkindate").val().trim();
+	let dateOb = new Date(checkinDate);
 
-	// if (venueName != "") {
+	// grab usernames
+	let username = localStorage.getItem("username");
+	let additionalUsers = $(".addusername");
+	console.log(dateOb.toISOString());
+	let usernames = [];
+	usernames.push(username);
+	additionalUsers.each(function () {
+		let newUser = $(this);
+		usernames.push(newUser.val());
+	});
+	usernames = $.uniqueSort(usernames);
+	// send the sanitized usernames to the api
+	validateUsersBeforeCheckin(usernames, dateOb);
+}
+function addUser() {
+	let container = $("#addusercon");
+	let template = $(
+		'<div class="container">' +
+			'<input type="text" class="form-control mb-3 w-25 addusername" placeholder="Name...">' +
+			"</div>"
+	);
+	template.appendTo(container);
+}
+
+function validateUsersBeforeCheckin(usernames, dateOb) {
+	// check to see which users actually exist
 	$.ajax({
 		type: "POST",
-		url: "https://mcn1g15-cw1.azurewebsites.net/api/checkIn",
+		url: "https://mcn1g15-cw1.azurewebsites.net/api/validateUsers",
 		data: JSON.stringify({
-			venueName: venue.Name,
-			username: localStorage.getItem("username"),
+			users: usernames,
 		}),
-		success: (response) => {
+		success: (r) => {
+			let response = JSON.parse(r);
 			console.log(response);
-			// getVenues();
+			// then make a checkin for each of them at the specified venue
+			$.ajax({
+				type: "POST",
+				url: "https://mcn1g15-cw1.azurewebsites.net/api/checkIn",
+				data: JSON.stringify({
+					venueName: venue.Name,
+					users: response.validUsers,
+					date: dateOb.toISOString(),
+				}),
+				success: (r2) => {
+					let response2 = JSON.parse(r2);
+					console.log(response2);
+					let success = $("#successcon");
+					let checkin = $("#checkincon");
+					checkin.hide();
+					success.show();
+				},
+			});
 		},
 	});
 }
-//  else {
-// }
